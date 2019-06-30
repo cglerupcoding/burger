573194492833c70542792ad58fde8084bc731011
chris <cglerup@gmail.com>
2019-06-29 19:32:30 -0500

    removed extra handlebar files

--- server.js
+++ server.js
@@ -1,33 +1,27 @@
-//Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
-
-//Define port the server will be listening on.
+var methodOverride = require("method-override");
+var exphbs = require("express-handlebars");
+var app = express();
 var PORT = process.env.PORT || 3000;
 
-var app = express();
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + "/public"));
 
-//Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + '/public'));
-
-//Parse application/x-www-form-urlencoded
+// Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//Parse application/json
-app.use(bodyParser.json());
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
 
-//Set Handlebars.
-var exphbs = require("express-handlebars");
-
+//Select and set Handlebars as the engine and "main" as the default layout
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/burgersController.js");
+//Imports the routes from burgers_controller
+var routes = require('./controllers/burgers_controller.js');
+app.use('/', routes);
 
-app.use(routes);
-
-//App is listening...
-app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
